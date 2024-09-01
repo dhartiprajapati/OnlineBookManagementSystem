7c24035ed993e29a94f1e910af5cc1cf3f92cbc8
dhartiprajapati <167629967+dhartiprajapati@users.noreply.github.com>
2024-09-01 14:06:15 +0100

    second Commit

---
+++ src/main/java/com/mycompany/onlinebookmanagementsystem/OnlineBookManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.onlinebookmanagementsystem;
+
+/**
+ *
+ * @author Algorithms Solutions
+ */
+public class OnlineBookManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
